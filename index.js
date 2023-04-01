2f6f77b450a2f378342262ff067c68dddce7141f
unknown <fictionary88@gmail.com>
2023-04-01 14:05:21 -0500

    Added actual API call

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ const corsOptions = {
   origin: ["http://localhost:3000", "https://ccjs.onrender.com"]
 };
 
+const requestEndpoint = "https://signup.com/api/events?accesskey=13fcbcd593bef760aaa4feeea1f7d14424466e1a&activity_id=3424432&enddate=2023%2F06%2F30&include_comments=false&include_jobassignments=true&include_jobs=true&my_jobs=false&selected_activity=3424432&startdate=2020%2F10%2F19";
+
 app.get('/', cors(corsOptions), (req, res) => {
-  res.send('Hello World! Now lets try to import node fetch @ 2');
+  res.send('Hello World! Added seperate async call and the url');
+})
+
+app.get('/getData', cors(corsOptions), async (req, res) => {
+  const fetchOptions = {
+      method: 'GET'
+  }
+  const response = await fetch(requestEndpoint, fetchOptions);
+  const jsonResponse = await response.json();
+  res.json(jsonResponse);
 })
 
 app.listen(port, () => {
