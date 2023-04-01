869766fdeb9b1a3d252cf7c03e4f0eb14ca3adb7
unknown <fictionary88@gmail.com>
2023-04-01 13:50:24 -0500

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://ccjs.onrender.com"]
+};
+
+app.get('/', cors(corsOptions), (req, res) => {
   res.send('Hello World!');
 })
 
