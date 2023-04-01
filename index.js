2d96612007fcafc8244fb8ae7339dfa3b351f82d
unknown <fictionary88@gmail.com>
2023-04-01 14:10:50 -0500

    Finished 1st Draft server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
 const requestEndpoint = "https://signup.com/api/events?accesskey=13fcbcd593bef760aaa4feeea1f7d14424466e1a&activity_id=3424432&enddate=2023%2F06%2F30&include_comments=false&include_jobassignments=true&include_jobs=true&my_jobs=false&selected_activity=3424432&startdate=2020%2F10%2F19";
 
 app.get('/', cors(corsOptions), (req, res) => {
-  res.send('Hello World! Added seperate async call and the url');
+  res.send('Hello World! Welcome to my server :)');
 })
 
 app.get('/getData', cors(corsOptions), async (req, res) => {
