84fe721214963bc81e9e0dce222be39f7665127c
Chris <fictionary88@gmail.com>
2023-07-02 12:34:12 -0500

    Subtract 5 hours so server in CST

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
 const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault("America/Chicago");
-dayjs().format();
 
-const dynamicRequest = (days, endDate) => {
-  const start = dayjs().subtract(days, 'days');
+const dynamicRequest = (days, now, endDate) => {
+  const start = now.subtract(days, 'days');
   const startStr = `${start.year()}%2F${start.month()+1}%2F${start.date()}`;
 
   return `https://signup.com/api/events?accesskey=13fcbcd593bef760aaa4feeea1f7d14424466e1a&activity_id=3424432&enddate=${endDate}&include_comments=false&include_jobassignments=true&include_jobs=true&my_jobs=false&selected_activity=3424432&startdate=${startStr}`;
@@ -22,8 +16,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   const now = dayjs();
-  res.locals.now = now;
-  res.locals.start = now.subtract(7, 'days');
+  res.locals.now = now.subtract(5, 'hours');
   res.locals.endDate = `${now.year()}%2F${now.month()+1}%2F${now.date()}`;
 
   next();
@@ -36,24 +29,31 @@ const corsOptions = {
 app.get('/', cors(corsOptions), (req, res) => {
   res.send(`
   <div>Hello World! Welcome to my server :)</div>
-  <div>Server time: ${req.res.locals.now.format()}</div>
+  <div>Server time in CST: ${req.res.locals.now.format()}</div>
   `);
 })
 
 app.get('/getTodaysData', cors(corsOptions), async (req, res) => {
+  const now = req.res.locals.now;
+  const endDate = req.res.locals.endDate;
+  
   const fetchOptions = {
       method: 'GET'
   }
-  const response = await fetch(dynamicRequest(0, req.res.locals.endDate), fetchOptions);
+  const response = await fetch(dynamicRequest(0, now, endDate), fetchOptions);
   const jsonResponse = await response.json();
   res.json(jsonResponse);
 })
 
 app.get('/getHistoricalData', cors(corsOptions), async (req, res) => {
+  const now = req.res.locals.now;
+  const endDate = req.res.locals.endDate;
+  const days = req.query.days;
+
   const fetchOptions = {
       method: 'GET'
   }
-  const response = await fetch(dynamicRequest(req.query.days, req.res.locals.endDate), fetchOptions);
+  const response = await fetch(dynamicRequest(days, now, endDate), fetchOptions);
   const jsonResponse = await response.json();
   res.json(jsonResponse);
 })
